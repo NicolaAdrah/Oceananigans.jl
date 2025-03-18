bb846f6dbfbf569fcccf1b417903a48e7cf2f973
Simone Silvestri <silvestri.simone0@gmail.com>
2025-03-18 19:52:38 +0100

    first (non running) protorype

--- src/BoundaryConditions/boundary_condition.jl
+++ src/BoundaryConditions/boundary_condition.jl
@@ -87,7 +87,7 @@ const ZBC  = BoundaryCondition{<:Zipper}
 # More readable BC constructors for the public API.
                 PeriodicBoundaryCondition() = BoundaryCondition(Periodic(),                 nothing)
                   NoFluxBoundaryCondition() = BoundaryCondition(Flux(),                     nothing)
-            ImpenetrableBoundaryCondition() = BoundaryCondition(Open(), nothing)
+            ImpenetrableBoundaryCondition() = BoundaryCondition(Open(),                     nothing)
 MultiRegionCommunicationBoundaryCondition() = BoundaryCondition(MultiRegionCommunication(), nothing)
 DistributedCommunicationBoundaryCondition() = BoundaryCondition(DistributedCommunication(), nothing)
                   ZipperBoundaryCondition() = BoundaryCondition(Zipper(), 1) # 1 means that the sign will not be switched
