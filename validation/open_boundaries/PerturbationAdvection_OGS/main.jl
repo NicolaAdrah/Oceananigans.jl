b7435b02012fc56a3f10d1fb0a3c8985a850dd89
NicolaAdrah <niccolo1996.na@gmail.com>
2025-09-16 10:30:18 +0200

    Modifying plotting

--- validation/open_boundaries/PerturbationAdvection_OGS/main.jl
+++ validation/open_boundaries/PerturbationAdvection_OGS/main.jl
@@ -17,8 +17,8 @@ grid = RectilinearGrid(CPU();
     z        = (-10, 0)
 )
 
-uB_east = Field{Nothing, Center, Center}(grid);
-uB_west = Field{Nothing, Center, Center}(grid);
+uB_east = Field{Nothing, Nothing, Center}(grid);
+uB_west = Field{Nothing, Nothing, Center}(grid);
 
 u_bcs = FieldBoundaryConditions(
     east = OpenBoundaryCondition(uB_east; scheme = PerturbationAdvection()),
@@ -56,12 +56,11 @@ simulation = Simulation(model; Δt = 5minutes, stop_time = 5days)
 # )
 
 η = model.free_surface.η
-simulation.output_writers[:tracer] = JLD2Writer(model, (; η,),
+simulation.output_writers[:free_surface] = JLD2Writer(model, (; η,),
     schedule            = TimeInterval(time_int),
     filename            = joinpath(output_dir, "free_surface.jld2"),
     overwrite_existing  = true
 )
-
 
 @info "Running..." 
 run!(simulation)
@@ -72,42 +71,21 @@ using Oceananigans.OutputReaders: FieldTimeSeries
 using Oceananigans.Grids: nodes
 using Oceananigans.Fields: interior
 
-# c_tr = FieldTimeSeries(joinpath(output_dir, "tracer.jld2"), "c")
-# Nt   = length(c_tr.times)
-η_tr = FieldTimeSeries(joinpath(output_dir, "free_surface.jld2"), "η")
-Nt   = length(η_tr.times)
+η = FieldTimeSeries(joinpath(output_dir, "free_surface.jld2"), "η")
 
-xC, _, zC = nodes(model.grid, Center(), Center(), Center())
-Nx = length(xC)
+Nt   = length(η.times)
 
-n      = Observable(1)
-# title_obs = @lift("Tracer c — t = $(round(c_tr.times[$n]/hour, digits=2)) h")
-title_obs = @lift("Free surface η — t = $(round(η_tr.times[$n]/hour, digits=2)) h")
+fig = Figure(resolution = (1000, 500))
+axη = Axis(fig[1, 1], title = "free surface")
 
-# 2D tracer slice (x–z, at y=1 since it's Flat)
-# c_slice = @lift(Array(interior(c_tr[$n]))[:, 1, :])
-η_slice = @lift(Array(interior(η_tr[$n]))[:, 1, :])
+n = Observable(1)
+ηn = @lift(interior(η[$n], :, 1, 1))
 
-# c_line  = @lift(interior(c_tr[$n], :, 1, 1))
-η_line  = @lift(interior(η_tr[$n], :, 1, 1))
-
-fig = Figure(resolution = (1000, 800))
-
-ax1 = Axis(fig[1, 1],
-           xlabel = "x (km)", ylabel = "z (m)",
-           title  = title_obs)
-# hm = heatmap!(ax1, xC ./ kilometer, zC, c_slice)
-# Colorbar(fig[1, 2], hm, label = "c")
-
-# ax2 = Axis(fig[2, 1], xlabel = "x (km)", ylabel = "c",
-#            title = "Tracer bump at y=1, z=1")
-# lines!(ax2, xC ./ kilometer, c_line)
-ax2 = Axis(fig[2, 1], xlabel = "x (km)", ylabel = "c",
-           title = "Free Surface bump at y=1, z=1")
-lines!(ax2, xC ./ kilometer, η_line)
+lines!(axη, ηn)
+ylims!(axη, (-0.1, 0.2))
 
 mp4file = joinpath(output_dir, "free_surface.mp4")
-record(fig, mp4file, 1:Nt; framerate = 12) do i
+record(fig, mp4file, 1:Nt) do i
     @info "frame $i / $Nt"
     n[] = i
 end
